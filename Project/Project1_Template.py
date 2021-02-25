f6aaca0cec54f5a7f221b94485fc8eabdd772ec3
Ronald Lagos <55566650+ronlagss@users.noreply.github.com>
2021-02-25 13:21:13 -0500

    Add files via upload

---
+++ Project/Project1_Template.py
@@ -0,0 +1,47 @@
+#Do not import any additional modules
+import numpy as np
+from PIL.Image import open
+import matplotlib.pyplot as plt
+
+### Load, convert to grayscale, plot, and resave an image
+I = np.array(open('Iribe.jpg').convert('L'))/255
+
+plt.imshow(I,cmap='gray')
+plt.axis('off')
+plt.show()
+
+plt.imsave('test.png',I,cmap='gray')
+
+### Part 1
+def gausskernel(sigma):
+    #Create a 3*sigma x 3*sigma 2D Gaussian kernel
+    h=np.array([[1.]])
+    return h
+
+def myfilter(I,h):
+    #Appropriately pad I
+    #Convolve I with h
+    I_filtered=I
+    return I_filtered
+
+
+h1=np.array([[-1/9,-1/9,-1/9],[-1/9,2,-1/9],[-1/9,-1/9,-1/9]])
+h2=np.array([[-1,3,-1]])
+h3=np.array([[-1],[3],[-1]])
+
+
+### Part 2
+Sx=np.array([[1,0,-1],[2,0,-2],[1,0,-1]])
+Sy=np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
+def myCanny(I,sigma=1,t_low=.5,t_high=1):
+    #Smooth with gaussian kernel
+    #Find img gradients
+    #Thin edges
+    #Hystersis thresholding
+    from scipy.ndimage.measurements import label
+    myedges=I
+    return myedges
+
+edges=myCanny(I,sigma=3,t_low=.5,t_high=1)
+plt.imshow(edges, interpolation='none')
+plt.show()
